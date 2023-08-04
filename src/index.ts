58da16bbfbb8fbb4a70c9862612e92db2506c88a
Franco <fcanalejo2000@outlook.com>
2023-08-05 02:49:55 +1000

    Delete webpack and unused dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
 import 'module-alias/register';
+import { select } from '@inquirer/prompts';
+import { textSync } from 'figlet';
+
+const questions = [
+    {
+        name: 'fromEditor',
+        message: 'Which editor are you converting from?',
+        choices: [
+            { name: 'Dreamweaver', value: 'dreamweaver', description: "" },
+            { name: 'Sublime', value: 'sublime', description: "" },
+            { name: 'VS Code', value: 'vscode', description: "" },
+            { name: 'Atom', value: 'atom', description: "" }
+        ]
+    },
+    {
+        name: 'toEditor',
+        message: 'Which editor are you converting to?',
+        choices: [
+            { name: 'Dreamweaver', value: 'dreamweaver', description: "" },
+            { name: 'Sublime', value: 'sublime', description: "" },
+            { name: 'VS Code', value: 'vscode', description: "" },
+            { name: 'Atom', value: 'atom', description: "WARN: Snippets scope are going to be converted to global" }
+        ]
+    }
+];
+
+const main = async () => {
+    console.log(textSync('C-Snippet', { horizontalLayout: 'full', font: 'ANSI Shadow' }), '\n');
+    const fromEditor = await select(questions[0]);
+    const toEditor = await select(questions[1]);
+    console.log(fromEditor, toEditor);
+}
+
+main();
