1853b23da4d70b93e375020b73799c35628e0a2f
Franco <fcanalejo2000@outlook.com>
2023-08-05 01:40:28 +1000

    Add webpack plugins and edit build command

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,50 @@ const TerserPlugin = require('terser-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const WebpackBar = require('webpackbar');
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+const SizePlugin = require('size-plugin');
 
-const WBarOptions = {
-  name: "🚚  C Snippet Bundle",
-  color: "#f00"
-}
+const smp = new SpeedMeasurePlugin();
 
-const TerserOptions = {
-  terserOptions: {
-    ecma: 2015,
-    compress: {
-      drop_console: false,
-    },
-    output: {
-      comments: false,
-    },
+const aliases = {
+  '@root': path.resolve(__dirname, 'src'),
+  '@utils': path.resolve(__dirname, 'src/utils'),
+  '@interfaces': path.resolve(__dirname, 'src/types/types.ts'),
+};
+
+const terserOptions = {
+  ecma: 2015,
+  compress: {
+    drop_console: false,
   },
-}
+  output: {
+    comments: false,
+  },
+};
 
-const Loaders = [
+const analyzerOptions = {
+  analyzerMode: 'static',
+};
+
+const loaders = [
   {
     test: /\.tsx?$/,
     loader: 'ts-loader',
     exclude: /node_modules/,
   },
-]
+];
 
-module.exports = {
+const plugins = [
+  new SizePlugin(),
+  new BundleAnalyzerPlugin(analyzerOptions),
+  new CleanWebpackPlugin(),
+  new WebpackBar({
+    name: '🚚  C Snippet Bundle',
+    color: '#0f0',
+  }),
+];
+
+const webpackConfig = smp.wrap({
   // mode: 'development', // or 'production'
   devtool: 'inline-source-map',
   entry: './src/index.ts',
@@ -39,6 +56,7 @@ module.exports = {
     path: path.resolve(__dirname, 'lib'),
     libraryTarget: 'commonjs2', // Set the library target to CommonJS2
   },
+  plugins,
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
     fallback: {
@@ -47,17 +65,15 @@ module.exports = {
       tls: false, // Disable the tls polyfill
       path: require.resolve('path-browserify'),
     },
+    alias: aliases,
   },
   optimization: {
     minimize: true,
-    minimizer: [
-      new TerserPlugin(TerserOptions),
-      new BundleAnalyzerPlugin(),
-      new CleanWebpackPlugin(),
-      new WebpackBar(WBarOptions)
-    ], 
+    minimizer: [new TerserPlugin({ terserOptions })],
   },
   module: {
-    rules: Loaders,
+    rules: loaders,
   },
-};
+});
+
+module.exports = webpackConfig;
