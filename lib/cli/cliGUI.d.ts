ae1d30a4c280812a9369e36655326e41ed4cbd2b
Franco <fcanalejo2000@outlook.com>
2023-11-07 21:23:41 +1000

    Deleted build folder

--- lib/cli/cliGUI.d.ts
+++
@@ -1,6 +0,0 @@
-import { CliArgs } from '../types/types';
-/**
- * Entry point of the CLI application.
- */
-declare const main: (params?: CliArgs) => Promise<void>;
-export default main;
--- lib/utils/spinner.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const spinner: import("ora").Ora;
